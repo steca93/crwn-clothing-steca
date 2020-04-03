4e6e652f22c41212cca7bd196275d1c22b466277
unknown <steca93@gmail.com>
2020-04-04 00:30:35 +0200

    implemented firebase utils included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -6,20 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
-
-// function App() {
-//   return (
-//     <div>
-//     <Header/>
-//       <Switch>
-//         <Route exact path='/' component={HomePage}/>
-//         <Route path='/shop' component={ShopPage}/>
-//         <Route path='/signin' component={SignInAndSignUpPage}/>
-//       </Switch>
-//     </div>
-//   );
-// }
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -33,10 +20,21 @@ class App extends React.Component {
   unsubscrubeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscrubeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsubscrubeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     })
   }
 
